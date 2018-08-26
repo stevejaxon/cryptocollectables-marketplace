8e5d68b4a0c86afb72b98a21feeb4a677901de49
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-08-26 15:13:53 +0100

    Aligning with the ERC-721 metadata standards

--- ts/MetadataAttribute.ts
+++ ts/MetadataAttribute.ts
@@ -1,9 +1,9 @@
 export class MetadataAttribute {
     public readonly trait_type: string;
     public readonly value: any;
-    public readonly display_type: string;
+    public readonly display_type?: string;
     public readonly max_value?: any;
-    constructor(traitType: string, attributeValue: any, displayType: string, maxValue?: any) {
+    constructor(traitType: string, attributeValue: any, displayType?: string, maxValue?: any) {
         this.trait_type = traitType;
         this.value = attributeValue;
         this.display_type = displayType;
