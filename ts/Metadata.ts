ba5920b5c12f1dbd98718d7564aad285392bc6de
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-08-26 15:13:32 +0100

    Aligning with the ERC-721 metadata standards

--- ts/Metadata.ts
+++ ts/Metadata.ts
@@ -1,18 +1,22 @@
 import {MetadataAttribute} from "./MetadataAttribute";
 
 export class Metadata {
+    public readonly id: number;
     public readonly description: string;
-    public readonly externalUrl: string;
+    public readonly external_url: string;
     public readonly image: string;
     public readonly name: string;
     public readonly attributes: [MetadataAttribute];
+    public readonly background_color?: string;
 
-    constructor(public itemDescription: string, public itemExternalUrl: string, public itemImageUrl: string,
-                public itemName: string, public itemAttributes: [MetadataAttribute]) {
+    constructor(idValue: number, itemDescription: string, itemExternalUrl: string, itemImageUrl: string,
+                itemName: string, itemAttributes: [MetadataAttribute], backgroundColor?: string) {
+        this.id = idValue;
         this.description = itemDescription;
-        this.externalUrl = itemExternalUrl;
+        this.external_url = itemExternalUrl;
         this.image = itemImageUrl;
         this.name = itemName;
         this.attributes = itemAttributes;
+        this.background_color = backgroundColor;
     }
 }
