02dd0c00753e1be5a4752296beb6620ce5aca4f8
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-08-31 23:26:11 +0100

    Adding functionality to generate an attribute with a value in a range. Includes simple test.

--- ts/MetadataGenerator.ts
+++ ts/MetadataGenerator.ts
@@ -1,4 +1,6 @@
+import {MetadataAttribute} from "./MetadataAttribute";
 import {MetadataAttributeEnum} from "./MetadataAttributeEnum";
+import {MetadataAttributeMetadata} from "./MetadataAttributeMetadata";
 
 export class MetadataGenerator {
     public selectTraitValue(traitType: string, enumMap: Map<string, MetadataAttributeEnum[]>): string {
@@ -15,5 +17,30 @@ export class MetadataGenerator {
             return enums[index].traitValue;
         }
         throw new Error("Invalid Enum");
+    }
+
+    public generateMetadataAttribute(traitAttributeMetadata: MetadataAttributeMetadata): MetadataAttribute | undefined {
+        if (this.isTraitExpressed(traitAttributeMetadata)) {
+            return this.generateMetadataAttributeWithBoundedValue(traitAttributeMetadata);
+        }
+        return undefined;
+    }
+
+    private generateMetadataAttributeWithBoundedValue(traitAttributeMetadata: MetadataAttributeMetadata): MetadataAttribute {
+        const value = this.selectValueInRange(traitAttributeMetadata.minValue || 0, traitAttributeMetadata.maxValue || 1);
+        return new MetadataAttribute(traitAttributeMetadata.traitType, value, traitAttributeMetadata.displayType, traitAttributeMetadata.maxValue);
+    }
+
+    private selectValueInRange(min: number, max: number): number {
+        if (min > max) {
+            return 0;
+        } else if (min === max) {
+            return min;
+        }
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    private isTraitExpressed(traitAttributeMetadata: MetadataAttributeMetadata): boolean {
+        return Math.random() <= traitAttributeMetadata.probability;
     }
 }
--- ts/__tests__/metadataGenerator.test.ts
+++ ts/__tests__/metadataGenerator.test.ts
@@ -2,6 +2,7 @@ import {MetadataGenerator} from "../MetadataGenerator";
 import {PropertiesDeserialiser} from "../PropertiesDeserialiser";
 
 const pathToEnumDataFile = "../../test/data/simple_metadata_attribute_enums.json";
+const pathToAttributeDataFile = "../../test/data/simple_metadata_attributes_metadata.json";
 
 test("it is possible to get a random trait from an enum", async () => {
     const dataRetrieved = await PropertiesDeserialiser.readAttributesDataFromFile(pathToEnumDataFile);
@@ -30,4 +31,18 @@ test("distribution is roughly as expected", async () => {
     expect(traitValueMap.get("common")).toBeGreaterThan(traitValueMap.get("rare") || 0);
     expect(traitValueMap.get("rare")).toBeGreaterThan(traitValueMap.get("epic") || 0);
     expect(traitValueMap.get("epic")).toBeGreaterThan(traitValueMap.get("legendary") || 0);
+});
+
+/**
+ * Calculating attribute values based on the details read from a file.
+ * Possible will be moved to its own suite of tests as it grows.
+ */
+
+test("can generate a simple attribute value based on a range", async () => {
+    const dataRetrieved = await PropertiesDeserialiser.readMetadataAttributesMetadataFromFile(pathToAttributeDataFile);
+    const metadataShape = dataRetrieved[0];
+    const metadata = new MetadataGenerator().generateMetadataAttribute(metadataShape);
+    expect(metadata).toBeDefined();
+    expect(metadata.value).toBeGreaterThanOrEqual(metadataShape.minValue || 99999);
+    expect(metadata.value).toBeLessThanOrEqual(metadataShape.maxValue || -1);
 });
